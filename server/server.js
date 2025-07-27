53ca5027f4ad2c95c10577ac49499ee5cd4abf35
Gelivi Naga Sowmyanath Gupta <118813994+040422@users.noreply.github.com>
2025-07-27 20:48:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use(cors({
+  origin:["http://localhost:3001","https://mern-stack-ai.netlify.app"]
+}))
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
